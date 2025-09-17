f73cd30251e1948b837c22f966c8f091c42bedc0
Raushan Kumar <raushankcse@gmail.com>
2025-09-17 22:25:03 +0530

    build

---
+++ week1/First-Gradle/src/main/java/org/utils/MyUtils.java
@@ -0,0 +1,8 @@
+package org.utils;
+
+public class MyUtils {
+
+    public static void getString(){
+        System.out.println("hiiii get your string");
+    }
+}
---
+++ week2/src/Day8/ArrayLists.java
@@ -0,0 +1,10 @@
+package Day8;
+
+import java.util.ArrayList;
+
+
+public class ArrayLists {
+  public static void main(String[] args) {
+    
+  }
+}
